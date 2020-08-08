b4c653e0ef6b7e86b60d3c58156c3c3ea7cda426
ezequiel3434 <ezequiel3434@gmail.com>
2020-08-08 15:16:45 -0300

    initial commit

---
+++ ButtonEffectsExtension/UIButtonExtension.swift
@@ -0,0 +1,16 @@
+//
+//  UIButtonExtension.swift
+//  ButtonEffectsExtension
+//
+//  Created by Ezequiel Parada Beltran on 08/08/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    // Borde redondo
+    func round(){
+        
+    }
+}
